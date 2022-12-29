b4b4bea3152e8c76107198acd8762528c10ab0f1
13aksh <1392aksh@gmail.com>
2022-12-29 19:19:08 +0530

    iterative solution

--- python/climbing_stairs.py
+++ python/climbing_stairs.py
@@ -1,11 +1,9 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        ways = {0: 0, 1: 1, 2: 2}
-
-        def steps(n: int) -> int:
-            if n < len(ways):
-                return ways[n]
-            ways[n] = steps(n - 1) + steps(n - 2)
-            return ways[n]
-
-        return steps(n)
+        stairs = [0, 1, 2]
+        if n > 2:
+            for i in range(2, n + 1):
+                stairs.append(stairs[i] + stairs[i - 1])
+            return stairs[i]
+        else:
+            return stairs[n]
